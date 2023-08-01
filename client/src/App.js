b0445c9c579e363b499478ce8124826c42e71b81
hamzaguyo725 <hamzaguyo725@gmail.com>
2023-08-01 10:22:47 +0300

    completed assignment

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import Footer from "./components/footer/footer";
+import SignupPage from "./components/signup/Signup";
+import { Route } from "react-router-dom/cjs/react-router-dom.min";
+import LoginPage from "./components/login/Login";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Route exact path="/">
+        <Footer />
+      </Route>
+      <Route exact path="/">
+        <LoginPage />
+      </Route>
+
+      <Route path="/register">
+        <SignupPage />
+      </Route>
     </div>
   );
 }
