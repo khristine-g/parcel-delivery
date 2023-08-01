2fde7261eab8ccf72b6c0bcd19fa8880c696e358
Martin <joewwi03@gmail.com>
2023-08-01 09:44:57 +0300

    Post debudding

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  resources :parcels, only: [:index]
+  resources :parcels
+  resources :recievers, only: [:create]
+  post "/signup", to: "user#create"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+
+  # get "/auth", to: "user#show"
 end
