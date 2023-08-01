1d2376b50498f52c2fb9de7287b8b9fe6bdea98b
Martin <joewwi03@gmail.com>
2023-08-01 09:42:38 +0300

    Database seeding

--- server/db/seeds.rb
+++ server/db/seeds.rb
@@ -5,30 +5,31 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-User.create!(username: "User1", email: 'user1@example.com', password: 'password1')
-User.create!(username: "User2", email: 'user2@example.com', password: 'password2')
 
-# Seed data for Parcel model
-Parcel.create!(
-  user_id: 1,
-  destination: '123 Main St, City A, Country X',
-  status: 'pending',
-  present_location: 'Pickup Point 1',
-  weight: '2 kg'
-)
+# Sample seed data for users table
+users_data = [
+  { username: "Alice", password: "secure_password1", email: "alice@example.com", phone_number: 1112223333, address: "789 Oak Ave, City C", admin_tag: false },
+  { username: "AdminUser", password: "admin123", email: "admin@example.com", phone_number: 4445556666, address: "101 Admin St, City D", admin_tag: true },
+]
+User.create!(users_data)
 
-Parcel.create!(
-  user_id: 1,
-  destination: '456 Elm St, City B, Country Y',
-  status: 'delivered',
-  present_location: 'Delivery Hub B',
-  weight: '3 kg'
-)
+# Sample seed data for recievers table
+recievers_data = [
+  { username: "John Doe", email: "john@example.com", phone_number: 1234567890, address: "123 Main St, City A" },
+  { username: "Jane Smith", email: "jane@example.com", phone_number: 9876543210, address: "456 Elm St, City B" },
+]
+Reciever.create!(recievers_data)
 
-Parcel.create!(
-  user_id: 2,
-  destination: '789 Oak St, City C, Country Z',
-  status: 'in_transit',
-  present_location: 'Transit Point 2',
-  weight: '1 kg'
-)
+# Sample seed data for mails table
+mails_data = [
+  { user_id: 1, receiver_id: 2, content: "Hello, how are you doing?" },
+  { user_id: 2, receiver_id: 1, content: "Thanks for the package!" },
+]
+Mail.create!(mails_data)
+
+# Sample seed data for parcels table
+parcels_data = [
+  { weight: 5, password: "secure123", present_location: "Warehouse A", status: "In Transit", user_id: 1, destination: "Home Address", Type_of_shipment: "Express", Carrier: "Carrier X", journey_duration: "2 days", Package: 1, travel_distance: 150, Pick_up_date: Time.now, Departure_date: Time.now + 2.days, receiver_id: 2 },
+  { weight: 10, password: "delivery789", present_location: "Courier Center B", status: "Delivered", user_id: 2, destination: "Office Address", Type_of_shipment: "Standard", Carrier: "Carrier Y", journey_duration: "5 days", Package: 2, travel_distance: 300, Pick_up_date: Time.now - 3.days, Departure_date: Time.now - 1.day, receiver_id: 1 },
+]
+Parcel.create!(parcels_data)
